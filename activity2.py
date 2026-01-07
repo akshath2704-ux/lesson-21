9f63d6119659b2d45be627de08e7615d89d0bb31
akshath2704-ux <akshath2704@gmail.com>
2026-01-07 19:05:44 +0800

    initial commit

---
+++ activity2.py
@@ -0,0 +1,11 @@
+def match_words(words):
+    ctr=0
+    list=[]
+    for word in words:
+        if len(word)>1 and word[0]==word[-1]:
+            ctr+=1
+            list.append(word)
+    print("list of words with first and last character same\n",list)
+    return ctr
+count=match_words(['abc','cfc','xyz','aba','1221'])
+print("number of words having first and last character same:",count)
